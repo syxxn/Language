2f76d72e7acf1bd24fa74595ef310636f1236b4e
이승윤 <yyuunn17@naver.com>
2021-03-22 19:57:37 +0900

    2021-03-22 실습 과제

---
+++ JAVASCRIPT/코드잇/002 프로그래밍 맛보기 실습과제/칼로리 계산기.js
@@ -0,0 +1,12 @@
+// 칼로리 계산기
+
+let espressoCalories = 10;
+let milkCalories = 170;
+let chocoSyrupCalories = 50;
+let whippedCreamCalories = 60;
+
+// 메뉴별 칼로리 테스트
+console.log(espressoCalories); // 에스프레소 칼로리
+console.log(espressoCalories + milkCalories); // 라떼 칼로리
+console.log(espressoCalories + chocoSyrupCalories + milkCalories); // 모카 칼로리
+console.log(espressoCalories + chocoSyrupCalories + milkCalories + whippedCreamCalories); // 모카(휘핑 추가) 칼로리
