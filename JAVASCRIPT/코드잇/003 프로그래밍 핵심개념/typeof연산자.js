4837e67d0d45f75f1712d3cf2241d7f6d3a2a1b5
이승윤 <yyuunn17@naver.com>
2021-03-25 21:48:58 +0900

    2021-03-25

---
+++ JAVASCRIPT/코드잇/003 프로그래밍 핵심개념/typeof연산자.js
@@ -0,0 +1,9 @@
+let name = "Codeit";
+
+function sayHello() {
+  console.log("Hello");
+}
+
+console.log(typeof name); //string
+console.log(typeof sayHello()); //Hello \n undefined
+console.log(typeof sayHello); //function
