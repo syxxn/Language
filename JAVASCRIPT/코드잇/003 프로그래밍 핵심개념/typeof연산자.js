fff5c7d66e12d6daa4af2751eb950eea57075928
이승윤 <yyuunn17@naver.com>
2021-03-25 21:49:37 +0900

    .

--- JAVASCRIPT/코드잇/003 프로그래밍 핵심개념/typeof연산자.js
+++ JAVASCRIPT/코드잇/003 프로그래밍 핵심개념/typeof연산자.js
@@ -7,3 +7,5 @@ function sayHello() {
 console.log(typeof name); //string
 console.log(typeof sayHello()); //Hello \n undefined
 console.log(typeof sayHello); //function
+
+//typeof 연산자는 사칙연산자보다 우선순위가 높다
